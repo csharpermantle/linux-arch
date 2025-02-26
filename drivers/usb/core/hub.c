59c9134a62b7aa2def8e97ff125e41e3890f371d
Mingcong Bai <jeffbai@aosc.io>
2025-02-26 11:43:02 +0800

    AOSCOS: USB: core: only enable root_hub wakeup on MACH_LOONGSON64
    The previous attempt to fix USB remote wake on Loongson 7A platforms broke
    S3 suspend on at least the following device(s):
    
    - Lenovo ThinkPad P14s Gen 1 (20Y1CTO1WW)
    
    Where the device would wake up immediately after entering S3 suspend.
    Since this fix was really only intended as a quirk for Loongson 7A
    platforms, guard the additional calls to `usb_enable_remote_wakeup()' and
    `usb_disable_remote_wakeup()' within `CONFIG_MACH_LOONGSON64',
    corresponding to all 64-bit Loongson platforms (unless this also breaks
    non-7A devices, in which case we will tighten the conditions further).
    
    Fixes: "FROMLIST: USB: core: Enable root_hub's remote wakeup for wakeup sources"
    Signed-off-by: Mingcong Bai <jeffbai@aosc.io>
    
    Link: https://t.me/c/1109254909/816220
    Link: https://t.me/c/1109254909/820298
    Signed-off-by: Kexy Biscuit <kexybiscuit@aosc.io>
    Signed-off-by: Mingcong Bai <jeffbai@aosc.io>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -3523,7 +3523,9 @@ int usb_port_suspend(struct usb_device *udev, pm_message_t msg)
 			if (PMSG_IS_AUTO(msg))
 				goto err_wakeup;
 		}
+#if defined(CONFIG_MACH_LOONGSON64)
 		usb_enable_remote_wakeup(udev->bus->root_hub);
+#endif
 	}
 
 	/* disable USB2 hardware LPM */
@@ -3589,7 +3591,9 @@ int usb_port_suspend(struct usb_device *udev, pm_message_t msg)
 
 		if (udev->do_remote_wakeup) {
 			(void) usb_disable_remote_wakeup(udev);
+#if defined(CONFIG_MACH_LOONGSON64)
 			(void) usb_disable_remote_wakeup(udev->bus->root_hub);
+#endif
 		}
  err_wakeup:
 
